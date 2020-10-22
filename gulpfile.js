4ae49c8cc6d98edf45b4bafc6dd85945984aae36
heykb <root@gaf.com>
2020-10-22 16:10:12 +0800

    edit readme.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,111 @@
-var gulp = require('gulp');
-var gulpVersion = require('./onemap-verison.js');
-var gulpif = require("gulp-if")
-var clean = require('gulp-clean');
-var zip = require('gulp-zip');
-var through = require('through2');
-var gulpIgnore = require('gulp-ignore');
-
+var gulp = require("gulp");
+var gulpVersion = require("./plugin/onemap-verison.js");
+var gulpif = require("gulp-if");
+var clean = require("gulp-clean");
+var zip = require("gulp-zip");
+var through = require("through2");
+var gulpIgnore = require("gulp-ignore");
 
 var config = {
-  // 项目根路径
-  // srcRoot: 'test/',
-  // // 输出路径
-   outRoot: 'dist/',
-  // outRoot:"E:\\notes\\notes\\software\\apache-tomcat-8.5.57\\webapps\\space\\WEB-INF\\classes\\",
-  srcRoot:'E:/notes/work/antu/onemapV5.0/',
-  // 匹配符
-  srcPattern: ['**/*.html', '**/*.js', '!**/plugin/**/*','!**/thirdpart/**/*','!**/_nuxt/commons/**/*'],
-  // 是否zip包输出
-  outZipEnable: true,
-  // 是否禁止输出项目所有文件，true: 只输出srcPattern匹配的文件（大大节约时间）
-  outAllRepDisable: false
-}
-var options = {
-  //参数名称
-  paramName: '_',
-  //文件后缀
-  suffix: ['css', 'js', 'html'],
-  // 参数替换还是追加replace append
-  mode: 'append',
-  // version  guid timestamp
-  paramType: 'timestamp',
-  // 匹配模式为 xx [=|+|:] xx/xx.[suffix]
-  assignSymbols: ["=",":","+"],
-  // 只输出内容被修改的文件
-  onlyOutChanged:config.outAllRepDisable,
-  // 需要排除不添加版本号的链接（针对某些链接添加版本号参数会带来额外影响）
-  excludeLinks:['bootstrap-material-design.min.css'],
-  // 大文件提示 单位：kb
-  largeSize:200,
-  // 自动跳过大文件
-  autoSkipLargeFile:false
-}
-
-function log(message){
-	return through.obj(function (file, enc, callback) {
-    process.stdout.write('\r\x1b[K')
-    process.stdout.write('out to dist:'+file.path)
-		this.push(file);
-		return callback();
-	});
+    // 项目根路径
+    // srcRoot: 'test/',
+    // // 输出路径
+    outRoot: "dist/",
+    // outRoot:"E:\\notes\\notes\\software\\apache-tomcat-8.5.57\\webapps\\space\\WEB-INF\\classes\\",
+    srcRoot: "E:/notes/work/antu/onemapV5.0/",
+    // 匹配符
+    srcPattern: [
+        "**/*.html",
+        "**/*.js",
+        "!**/plugin/**/*",
+        "!**/thirdpart/**/*",
+        "!**/_nuxt/commons/**/*",
+    ],
+    // 是否zip包输出
+    outZipEnable: true,
+    // 极简模式，true: 只输出srcPattern匹配的文件
+    outAllRepDisable: false,
 };
-gulp.task('transform', function () {
-  if(config.outAllRepDisable){
-    config.srcPattern = config.srcPattern.map(item=>{
-      if(item.startsWith('!'))return '!'+config.srcRoot+'/'+item.substring(1);
-      else return config.srcRoot+'/'+item;
-    })
-    var stream = gulp.src(config.srcPattern)
-      .pipe(gulpif(['**/seajsInit.js', '**/initPortal.js'], gulpVersion.seajsVersion(options)))          
-      .pipe(gulpVersion.gulpVersion(options));
-    if (config.outZipEnable) {
-      stream = stream.pipe(zip('archive.zip'));
-    }
-    return stream.pipe(gulp.dest(config.outRoot))
-  }else{
-    var stream = gulp.src(config.srcRoot + '/**/*')
-          .pipe(gulpif(['**/seajsInit.js', '**/initPortal.js'], gulpVersion.seajsVersion(options)))
-          .pipe(gulpif(config.srcPattern,gulpVersion.gulpVersion(options),log()))
-    if (config.outZipEnable) {
-      stream = stream.pipe(zip('archive.zip'));
-    }
-    return stream.pipe(gulp.dest(config.outRoot))
-  }
-  
-});
+var options = {
+    //参数名称
+    paramName: "_",
+    //文件后缀
+    suffix: ["css", "js", "html"],
+    // 参数替换还是追加replace append
+    mode: "append",
+    // version  guid timestamp
+    paramType: "timestamp",
+    // 匹配模式为 xx [=|+|:] xx/xx.[suffix]
+    assignSymbols: ["=", ":", "+"],
+    // 只输出内容被修改的文件
+    onlyOutChanged: config.outAllRepDisable,
+    // 需要排除不添加版本号的链接（针对某些链接添加版本号参数会带来额外影响）
+    excludeLinks: ["bootstrap-material-design.min.css"],
+    // 大文件提示 单位：kb
+    largeSize: 200,
+    // 自动跳过大文件
+    autoSkipLargeFile: false,
+};
 
+function log(message) {
+    return through.obj(function (file, enc, callback) {
+        process.stdout.write("\r\x1b[K");
+        process.stdout.write("out to dist:" + file.path);
+        this.push(file);
+        return callback();
+    });
+}
+gulp.task("transform", function () {
+    if (config.outAllRepDisable) {
+        config.srcPattern = config.srcPattern.map((item) => {
+            if (item.startsWith("!"))
+                return "!" + config.srcRoot + "/" + item.substring(1);
+            else return config.srcRoot + "/" + item;
+        });
+        var stream = gulp
+            .src(config.srcPattern)
+            .pipe(
+                gulpif(
+                    ["**/seajsInit.js", "**/initPortal.js"],
+                    gulpVersion.seajsVersion(options)
+                )
+            )
+            .pipe(gulpVersion.gulpVersion(options));
+        if (config.outZipEnable) {
+            stream = stream.pipe(zip("archive.zip"));
+        }
+        return stream.pipe(gulp.dest(config.outRoot));
+    } else {
+        var stream = gulp
+            .src(config.srcRoot + "/**/*")
+            .pipe(
+                gulpif(
+                    ["**/seajsInit.js", "**/initPortal.js"],
+                    gulpVersion.seajsVersion(options)
+                )
+            )
+            .pipe(
+                gulpif(
+                    config.srcPattern,
+                    gulpVersion.gulpVersion(options),
+                    log()
+                )
+            );
+        if (config.outZipEnable) {
+            stream = stream.pipe(zip("archive.zip"));
+        }
+        return stream.pipe(gulp.dest(config.outRoot));
+    }
+});
 
 //清空目标文件
-gulp.task('cleanDist', function () {
-  return gulp.src(config.outRoot,
-    {
-      read: false,
-      allowEmpty: true
-    })
-    .pipe(clean());
+gulp.task("clean", function () {
+    return gulp
+        .src(config.outRoot, {
+            read: false,
+            allowEmpty: true,
+        })
+        .pipe(clean());
 });
 
-gulp.task('default', gulp.series('transform'));
+gulp.task("default", gulp.series("transform"));
---
+++ plugin/onemap-verison.js
@@ -0,0 +1,298 @@
+"use strict";
+var through = require("through2");
+var assign = require("object-assign");
+var gutil = require("gulp-util");
+var pkg = require(process.cwd() + "/package.json");
+var addSeajsConfigMap = require("./seajs-config-version.js");
+var minimatch = require("minimatch");
+
+// plugin name
+const PLUGIN_NAME = "gulp-html-version";
+
+// default parameter
+var defaults = {
+    paramName: "v",
+    paramType: "version",
+    suffix: ["css", "js"],
+    assignSymbols: ["="],
+    excludeFilenames: [],
+    onlyOutChanged: true,
+    largeSize: 200,
+    autoSkipLargeFile: false,
+    mode: "replace", // 'replace: replace version from the old version; append: append version information after the old version using &'
+};
+function Stack() {
+    this.item = [];
+    this.push = function (element) {
+        this.items.push(element);
+    };
+    this.pop = function () {
+        return this.items.pop();
+    };
+    this.pop = function () {
+        return this.items[this.items.length - 1];
+    };
+    this.isEmpty = function () {
+        return !this.items.length;
+    };
+    this.clear = function () {
+        this.items = [];
+    };
+    this.size = function () {
+        return this.items.length;
+    };
+}
+/**
+ * Short unique id generator
+ */
+var ShortId = function () {
+    var lastTime;
+
+    this.next = function () {
+        var d = new Date();
+        var thisTime =
+            (d.getTime() - Date.UTC(d.getUTCFullYear(), 0, 1)) * 1000;
+        while (lastTime >= thisTime) {
+            thisTime++;
+        }
+        lastTime = thisTime;
+        return thisTime.toString(16);
+    };
+};
+
+function gulpVersion(options) {
+    // merge
+    var opts = assign(defaults, options);
+    var shortId = new ShortId();
+
+    // switch a parameter
+    switch (opts.paramType) {
+        case "version":
+            opts.version = pkg.version;
+            break;
+        case "guid":
+            opts.version = shortId.next();
+            break;
+        case "timestamp":
+            opts.version = Date.now();
+            break;
+    }
+
+    var assignSymbols = opts.assignSymbols;
+    //([\w-"']+[\s]*[=]{1}[\s]*".+)(\.css|js|html)(\?([^&"']+(?:&[^&"']+)*))?(")
+    var regex = new RegExp(
+        "(([\\w-\"']+)[\\s]*([" +
+            assignSymbols.join("|") +
+            ']){1}[\\s]*"(.+(\\.css|js|html)))(\\?([^&"\']+(?:&[^&"\']+)*))?(")',
+        "ig"
+    );
+    // 单引号 fix:if user use single quotes for quote js css file,only use regex above may cause wrong regex
+    var regex2 = new RegExp(
+        "(([\\w-\"']+)[\\s]*([" +
+            assignSymbols.join("|") +
+            "]){1}[\\s]*'(.+(\\.css|js|html)))(\\?([^&\"']+(?:&[^&\"']+)*))?(')",
+        "ig"
+    );
+    var regex3 = new RegExp(
+        "([\\w-\"'`]+[\\s]*[" +
+            assignSymbols.join("|") +
+            "]{1}\\s*`.+)(\\." +
+            opts.suffix.join("|") +
+            ")(\\?([^&\"'`]+(?:&[^&\"'`]+)*))?(`)",
+        "ig"
+    );
+    // bootstrap-material-design.min.css
+    console.log(
+        "([\\w-\"']+[\\s]*[" +
+            assignSymbols.join("|") +
+            ']{1}\\s*".+)(\\.' +
+            opts.suffix.join("|") +
+            ')(\\?([^&"\']+(?:&[^&"\']+)*))?(")'
+    );
+    var stream = through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+
+        if (file.isStream()) {
+            this.emit(
+                "error",
+                new gutil.PluginError(PLUGIN_NAME, "Streaming not supported")
+            );
+            return cb();
+        }
+        process.stdout.write("\r\x1b[K");
+        process.stdout.write(`progress on: ${file.path} ... `);
+        if (file.stat.size / 1000 > opts.largeSize) {
+            process.stdout.write("\x1B[31m[large file] \x1B[0m");
+            if (opts.autoSkipLargeFile) {
+                process.stdout.write("\x1B[31m[skip] \x1B[0m\n");
+                if (!opts.onlyOutChanged) {
+                    this.push(file);
+                }
+                return cb();
+            }
+        }
+
+        var contents = file.contents.toString();
+        var matched = false;
+        // replace
+        contents = contents.replace(regex, function (
+            match,
+            $1,
+            left,
+            mid,
+            path,
+            suffix,
+            suffixRight,
+            param,
+            $7
+        ) {
+            for (var i = 0; i < opts.excludeFilenames.length; ++i) {
+                if (path.endsWith(opts.excludeFilenames[i])) {
+                    process.stdout.write(
+                        `\x1B[34m[eclude link:${match}] \x1B[0m`
+                    );
+                    return match;
+                }
+            }
+
+            matched = true;
+            var version;
+            // append parameter
+            if (param != undefined) {
+                if (opts.mode == "replace") {
+                    // replace version
+                    version = "?" + opts.paramName + "=" + opts.version;
+                } else {
+                    // append version
+                    version =
+                        "?" + opts.paramName + "=" + opts.version + "&" + param;
+                }
+            } else {
+                version = "?" + opts.paramName + "=" + opts.version;
+            }
+            return $1 + version + $7;
+        });
+        // 单引号处理
+        contents = contents.replace(regex2, function (
+            match,
+            $1,
+            left,
+            mid,
+            path,
+            suffix,
+            suffixRight,
+            param,
+            $7
+        ) {
+            for (var i = 0; i < opts.excludeFilenames.length; ++i) {
+                if (path.endsWith(opts.excludeFilenames[i])) {
+                    console.log(`\x1B[34m[eclude link:${match}] \x1B[0m`);
+                    return match;
+                }
+            }
+
+            matched = true;
+            var version;
+            // append parameter
+            if (param != undefined) {
+                if (opts.mode == "replace") {
+                    // replace version
+                    version = "?" + opts.paramName + "=" + opts.version;
+                } else {
+                    // append version
+                    version =
+                        "?" + opts.paramName + "=" + opts.version + "&" + param;
+                }
+            } else {
+                version = "?" + opts.paramName + "=" + opts.version;
+            }
+            return $1 + version + $7;
+        });
+        contents = contents.replace(regex3, function (
+            match,
+            $1,
+            $2,
+            $3,
+            $4,
+            $5
+        ) {
+            matched = true;
+            var version;
+            // append parameter
+            if ($3 != undefined) {
+                if (opts.mode == "replace") {
+                    // replace version
+                    version = "?" + opts.paramName + "=" + opts.version;
+                } else {
+                    // append version
+                    version =
+                        "?" + opts.paramName + "=" + opts.version + "&" + $4;
+                }
+            } else {
+                version = "?" + opts.paramName + "=" + opts.version;
+            }
+            return $1 + $2 + version + $5;
+        });
+        if ((opts.onlyOutChanged && matched) || !opts.onlyOutChanged) {
+            file.contents = Buffer.from(contents);
+            this.push(file);
+            if (matched) {
+                console.log("\x1B[32m[changed] \x1B[0m");
+            } else {
+                console.log("\x1B[33m[not changed but out] \x1B[0m");
+            }
+        } else {
+            console.log(`\x1B[33m[not changed] \x1B[0m`);
+        }
+        return cb();
+    });
+
+    return stream;
+}
+
+function seajsVersion(options) {
+    // merge
+    var opts = assign(defaults, options);
+    var shortId = new ShortId();
+
+    // switch a parameter
+    switch (opts.paramType) {
+        case "version":
+            opts.version = pkg.version;
+            break;
+        case "guid":
+            opts.version = shortId.next();
+            break;
+        case "timestamp":
+            opts.version = Date.now();
+            break;
+    }
+    var stream = through.obj(function (file, enc, cb) {
+        process.stdout.write(`seajsversion task on: ${file.path}... `);
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+
+        if (file.isStream()) {
+            this.emit(
+                "error",
+                new gutil.PluginError(PLUGIN_NAME, "Streaming not supported")
+            );
+            return cb();
+        }
+
+        var contents = file.contents.toString();
+        contents = addSeajsConfigMap(contents, opts);
+        file.contents = Buffer.from(contents);
+        this.push(file);
+
+        cb();
+    });
+    return stream;
+}
+
+module.exports = { gulpVersion, seajsVersion };
